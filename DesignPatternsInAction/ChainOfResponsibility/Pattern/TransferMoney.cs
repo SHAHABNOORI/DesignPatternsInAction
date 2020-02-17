b472d0ab3a232eff9a99d94d4e9fdf5ca6eff895
shahabnoori <noorigoodarzi@gmail.com>
2020-02-18 02:35:18 +0330

    Complete Chain Of Responsibility Pattern

---
+++ DesignPatternsInAction/ChainOfResponsibility/Pattern/TransferMoney.cs
@@ -0,0 +1,16 @@
+﻿using ChainOfResponsibility.Models;
+
+namespace ChainOfResponsibility.Pattern
+{
+    public abstract class TransferMoney
+    {
+        protected readonly TransferMoney Successor;
+
+        protected TransferMoney(TransferMoney successor)
+        {
+            Successor = successor;
+        }
+
+        public abstract ResponceContext Execute(RequestContext requestContext);
+    }
+}
---
+++ DesignPatternsInAction/ChainOfResponsibility/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using ChainOfResponsibility.Models;
+using ChainOfResponsibility.Pattern;
+
+namespace ChainOfResponsibility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var from = new Customer()
+            {
+                AccountValue = 1000,
+                Active = true,
+                MaxDateValue = 100,
+                Password = "1234"
+            };
+
+            var to = new Customer()
+            {
+                AccountValue = 10000,
+                Active = true,
+                MaxDateValue = 2000,
+                Password = "1234"
+            };
+
+            var transferPathOne = new CheckPassword(new CheckBallance(new CheckCustomerActiveState(new FinalizeTransfer(null))));
+
+            var result = transferPathOne.Execute(new RequestContext()
+            {
+                From = from,
+                Password = "1234",
+                Value = 50,
+                To = to
+            });
+
+            Console.WriteLine("Result :: " + result.Message);
+
+            Console.ReadKey();
+        }
+    }
+}
