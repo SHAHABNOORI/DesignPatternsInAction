0f480035e16b0d0479f45cc9571d301c36836b4e
shahabnoori <noorigoodarzi@gmail.com>
2020-03-04 10:53:33 +0330

    Initial Memento Pattern

---
+++ DesignPatternsInAction/MementoPattern/Car.cs
@@ -0,0 +1,47 @@
+﻿namespace MementoPattern
+{
+    public class Car
+    {
+        private int _fule;
+        private int _speed;
+
+        public Car()
+        {
+
+        }
+
+        public Car(CarState state)
+        {
+            _fule = state.Fule;
+            _speed = state.Speed;
+        }
+        public void AddFule(int value)
+        {
+            _fule += value;
+        }
+
+        public void IncreaseSpeed()
+        {
+            _speed++;
+        }
+
+        public void DecreaseSpeed()
+        {
+            _speed--;
+        }
+
+        public void Killometer()
+        {
+            _fule -= 6;
+        }
+
+        public CarState GetState()
+        {
+            return new CarState()
+            {
+                Fule = _fule,
+                Speed = _speed
+            };
+        }
+    }
+}
---
+++ DesignPatternsInAction/MementoPattern/CarState.cs
@@ -0,0 +1,9 @@
+﻿namespace MementoPattern
+{
+    public class CarState
+    {
+        public int Fule { get; set; }
+
+        public int Speed { get; set; }
+    }
+}
